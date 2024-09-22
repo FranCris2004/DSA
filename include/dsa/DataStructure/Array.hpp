558950464cd491801fe973b13962ec6e3265bdf6
Franco Cristaldo <francocristaldo2004@gmail.com>
2024-09-22 17:40:17 -0300

    Updated Array

--- include/dsa/DataStructure/Array.hpp
+++ include/dsa/DataStructure/Array.hpp
@@ -2,41 +2,41 @@
 
 #include <cstddef>
 #include <stdexcept>
+#include <array>
 
 namespace dsa
 {
-    /* Fixed size array
-     * 
-     * zero sized array not implemented, use it at your own risk
+    /**
+     * @brief Fixed size array
+     *
+     * @warning zero sized array not implemented, use it at your own risk
      */
-    template <typename _Tp, std::size_t _Sz>
+    template <typename Tp, std::size_t Sz>
     class Array
     {
     public:
         typedef std::size_t size_type;
-        typedef _Tp value_type;
-        typedef value_type &reference;
+        typedef Tp value_type;
 
-    private:
-        value_type m_data[_Sz];
-
-    public:
         static constexpr size_type size()
         {
-            return _Sz;
+            return Sz;
         }
 
-        constexpr reference operator[](size_type index)
+        inline value_type &operator[](size_type index)
         {
             return m_data[index];
         }
 
-        constexpr reference at(size_type index)
+        inline value_type &at(size_type index)
         {
             if (index >= this->size())
                 throw new std::out_of_range("Out of range index");
 
             return m_data[index];
         }
+
+    private:
+        value_type m_data[Sz];
     };
 } // namespace dsa
