135e07f517aeaac687476426b54f1844effe0e77
Franco Cristaldo <francocristaldo2004@gmail.com>
2024-09-24 23:30:17 -0300

    Array updated

--- test/Array.cpp
+++ test/Array.cpp
@@ -2,19 +2,24 @@
 #include <iostream>
 #include <dsa/DataStructure/Array.hpp>
 
+using namespace std;
+using namespace dsa;
+
+class Object
+{
+public:
+    Object() { cout << "constructor\n"; }
+    ~Object() { cout << "destructor\n"; }
+};
+
 int main()
 {
-    using namespace std;
-    using namespace dsa;
-
-    Array<int, 8> array;
+    Array<int, 3> array;
 
     array[0] = 1;    // ok
     array.at(1) = 1; // ok
-    /*
-    array.at(array.size()) = 1; // this throws an out of range exception
-    array[array.size()] = 1;    // this does not throws and exception (unsafe)
-    */
+    // array.at(array.size()) = 1; // this throws an out of range exception
+    // array[array.size()] = 1;    // this does not throws and exception (unsafe)
 
     for (size_t i = 0; i < array.size(); i++)
         array[i] = i * 2;
@@ -25,16 +30,22 @@ int main()
         array[i] = i * 2;
     */
 
+    cout << "array: {";
     for (size_t i = 0; i < array.size(); i++)
-        cout << i << "\n";
+        cout << i << ",";
+    cout << "}\n";
 
-    // 0 sized array
-    /*
-    Array<int, 0> arr0;
-    cout << arr0[0] << "\n";
-    arr0[0] = 1;
-    cout << arr0[0] << "\n";
-    */
+    // zero-sized array
+    Array<int, 0> zero_sized;
+
+    // zero_sized[0] = 1; // abort the execution
+    cout << zero_sized.size() << "\n";
+    cout << zero_sized.data() << "\n";
+
+    // object array
+
+    Array<Object, 3> object_array;
+
 
     return 0;
 }
